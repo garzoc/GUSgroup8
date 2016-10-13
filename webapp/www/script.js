d0aa72ed61d3130feeba0599f2515839829679b7
Unknown <funkyfisch@gmail.com>
2016-10-13 16:04:37 +0200

    JSON object creation

--- webapp/www/script.js
+++ webapp/www/script.js
@@ -1,21 +1,22 @@
 window.onload = function() {
      document.getElementById('Connect-Button').onclick=connectToNodeJS;
-     document.getElementById('Display-JSON').onclick=createJSONObject;
 }
 
 
 function connectToNodeJS() {
   document.getElementById('Connect-Button').innerHTML='Please wait';
-  var socket=new WebSocket("ws://localhost:8080/");
+  var socket=new WebSocket("ws://129.16.230.180:8000/");
 
   socket.onopen=function(e){
   	console.log("Establishing contact");
-    document.getElementById('Connect-Button')='Connection Successful';
+    document.getElementById('Connect-Button').innerHTML='Connection Successful';
   };
 
   socket.onmessage=function(e){
-  	var message=e.data.split("::");
-  	window[message[0]](message[1]);
+  	var message=e.data;
+    console.log(message);
+  	//window[message[0]](message[1]);
+
   };
 
   socket.onclose=function(){
@@ -26,6 +27,9 @@ function connectToNodeJS() {
 
 }
 
-function createJSONObject(receive) {
+function displayJSONObject(receive) {
   var obj = JSON.parse(receive);
+  document.getElementById('Sensor-ID').innerHTML=obj.id;
+  document.getElementById('Sensor-Value').innerHTML=obj.value;
+  document.getElementById('Time-Stamp').innerHTML=obj.time;
 }
