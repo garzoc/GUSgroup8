28792637a196ef855438912b0077cc9973bac8ae
Emanuel Mellblom <emanuel.mellblom@gmail.com>
2016-10-13 11:28:47 +0200

    Send random values
    Changed so that the test function send a generated value as payload instead of hardcoded string

--- broker_test.erl
+++ broker_test.erl
@@ -1,20 +1,26 @@
--module(broker_test).
+-module(mqttSender).
+-export([test/0]).
 
--export(test/1]).
+%qos_opt_test() ->
+ %  ?assertEqual({0, emqttc:qos_opt(value_generator:create_value()})).
 
-% Hardcoded IP for the time being
-test(Payload) -> 
+test() ->
+    %V = value_generator:create_value(),
+    R = io_lib:format("~p",[value_generator:create_value()]),
+    lists:flatten(R),
+
     {ok, C} = emqttc:start_link([
-		{host, "broker.hivemq.com"},
-		{port, 1883},
-		{client_id, <<"testClient">>}]),
-    emqttc:subscribe(C, <<"TestData">>, 0),
-    emqttc:publish(C, <<"TestTopic">>, <<"This is a test!">>),
+                {host, "broker.hivemq.com"},
+                {port, 1883},
+                {client_id, <<"testClientEmanuel">>}]),
+    emqttc:subscribe(C, <<"group8">>, 0),
+    emqttc:publish(C, <<"group8">>,list_to_binary(R)), %fun value_generator:create_value/0
     receive
         {publish, Topic, Payload} ->
             io:format("Message Received from ~s: ~p~n", [Topic, Payload])
     after
-        1000 ->
+        5000 ->
             io:format("Error: receive timeout!~n")
     end,
-    emqttc:disconnect(C).
+    emqttc:disconnect(C).  
+
