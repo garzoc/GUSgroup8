5162dff4fc625e272f3f282392af61fc98a98f01
Emanuel Mellblom <emanuel.mellblom@gmail.com>
2016-11-02 17:31:46 +0100

    Add files via upload

--- erlang_tcp.erl
+++ erlang_tcp.erl
@@ -1,17 +1,39 @@
 
 -module(erlang_tcp).
--export([test_tcp/0, send_times/3]).
+-export([test_tcp/1, send_times/3, init_server/0]).
  
-test_tcp() ->
+% Start the node.js server 
+init_server() ->
     {ok, Socket} = gen_tcp:connect({172,20,10,3}, 1337, [{mode, binary}]),
-    %Data = list_to_binary([25, "Hello"]),
+    Initmessage = "initProcess::{\"name\":\"hej\",\"type\":\"webServ\"}",
+    gen_tcp:send(Socket, Initmessage),
+    timer:sleep(5000),
+    test_tcp(Socket).
+
+% 
+test_tcp(Socket) ->
+    %init_server(),
+    %timer:sleep(1500),
+    %{ok, Socket} = gen_tcp:connect({172,20,10,3}, 1337, [{mode, binary}]),
     R = value_generator:create_value(),
-    Data = list_to_binary([25, json:decode(R)]),
+    %Data = lists:flatten(atom_to_list(json:decode(R))),
+    Data = atom_to_list(json:decode(R)),
+    %io:format("~w~n",[json:decode(R)]),
     send_times(0, Data, Socket),
+
+
     gen_tcp:close(Socket).
 
-send_times(3, Data, Socket) -> 
-    gen_tcp:send(Socket, Data);
-send_times(Acc, Data, Socket) -> io:format("hej~n"),
-    gen_tcp:send(Socket, Data), timer:sleep(110), 
+% Specify how many times a message should be sent
+send_times(15, Data, Socket) -> 
+    gen_tcp:send(Socket, atom_to_list(json:decode(value_generator:create_value())));
+send_times(Acc, Data, Socket) -> 
+    %io:format("~w~n~n~n",[Data]),
+    gen_tcp:send(Socket, atom_to_list(json:decode(value_generator:create_value()))), 
+    timer:sleep(3000), %110
     send_times(Acc+1, Data, Socket).
+
+% erlang_tcp:init_server().
+% erlang_tcp:test_tcp().
+
+
