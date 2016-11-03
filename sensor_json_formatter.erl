93f2105b822981877a029f32d7bcd85cb9fb770d
Isar Arason <gusarais@student.gu.se>
2016-11-03 14:59:25 +0100

    Added function to get data necessary to push to broker. Added missing Package field to JSON messages.

--- sensor_json_formatter.erl
+++ sensor_json_formatter.erl
@@ -2,10 +2,11 @@
 
 -module(sensor_json_formatter).
 -author("Isar Arason").
--export([sensor_to_json/2]).
+-export([sensor_to_json/2, sensor_to_broker_json/2, test/0]).
 
 sensor_to_json(Value, SensorName) ->
 	M = [
+			{package, config_accesser:get_field(package)},
 			{user, config_accesser:get_field(user)},
 			{group, config_accesser:get_field(group)},
 			{value, Value},
@@ -14,3 +15,20 @@ sensor_to_json(Value, SensorName) ->
 			{unit, config_accesser:get_sensor_unit(SensorName)}
 		],
 	json:decode(M).
+
+% Returns a json value formatter with the /Group/User/Package
+% as a tuple of atoms
+sensor_to_broker_json(Value, SensorName) ->
+    M = 
+		[
+			{value, Value}, 
+			{sensorID, SensorName}, 
+			{timestamp, erlang:system_time(seconds)}
+		],
+	User = config_accesser:get_field(user),
+	Group = config_accesser:get_field(group),
+	Package = config_accesser:get_field(package),
+    {Package, User, Group, json:decode(M)}.
+	
+test() ->
+	sensor_to_json(1, testSensor1).
