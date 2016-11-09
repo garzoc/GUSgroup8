81f44914cce86052a682014eb824bd9eb44a0ebc
garzoc <ost@airmail.cc>
2016-11-09 15:37:30 +0100

    minor changes

--- relay_listen.erl
+++ relay_listen.erl
@@ -1,6 +1,7 @@
 -module(relay_listen).
 -export([start/0,start/1,process/1]).
 -define(defPort,1337).
+-define(pFrequency,110).
 
 start() -> start(?defPort).
 
@@ -13,28 +14,28 @@ start(Port) ->
 server_loop(Listensocket) ->
 	case gen_tcp:accept(Listensocket) of
 		{ok,Socket} ->
-		spawn(?MODULE,process,[Socket]),
+		spawn(fun()-> process(Socket) end),
 		server_loop(Listensocket);
 		{error,Reason} ->
 		exit({accept,Reason})
 	end.
 %% handle current connection 	
 process(Socket) -> 
-	R = do_recv(Socket),
-	io:format("~p~n",[R]),
-	send_times(Acc,R,Socket),
+	Err = do_recv(Socket),
+	io:format("~p~n",[Err]),
 	gen_tcp:close(Socket).
 	
+send_times(0,_,_) ->ok;	
 send_times(Acc, Data, Socket) -> 
-    gen_tcp:send(Socket, atom_to_list(json:decode(value_generator:create_value()))), 
-    timer:sleep(3000), %110
+    gen_tcp:send(Socket,Data), 
+    timer:sleep(?pFrequency), %110
     send_times(Acc+1, Data, Socket).	
 	
 
 %% receive data from socket	
 do_recv(Socket) ->  
   case gen_tcp:recv(Socket, 0) of  
-    {ok, Bin} -> binary_to_list(Bin);  
+    {ok, Bin} -> send_times(1,binary_to_list(Bin),Socket),do_recv(Socket);  
     {error, closed} -> exit(closed);  
     {error, Reason} -> exit(Reason)  
   end.  	
