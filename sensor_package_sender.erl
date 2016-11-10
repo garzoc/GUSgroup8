76bf3cc8c729492bb7de7ead96862c7bfdbf3b16
Isar Arason <gusarais@student.gu.se>
2016-11-10 16:43:48 +0100

    Buffered sensor reading, sent to localhost

--- sensor_package_sender.erl
+++ sensor_package_sender.erl
@@ -4,22 +4,29 @@
 
 % Starts a process using IP and port in config file
 start() -> 
-	{ok, Socket} = gen_tcp:connect(
-		config_accesser:get_field(relay_ip),
-		config_accesser:get_field(relay_port),
-		[{mode, binary}]
-	),
-	Pid = spawn(fun() -> 
-			loop(
-			[], 
-			erlang:system_time(seconds),
-			Socket
-		) end
-	),
-	register(sender, Pid).
+	case whereis(sender) of
+		undefined ->
+			{ok, Socket} = gen_tcp:connect(
+				config_accesser:get_field(relay_ip),
+				config_accesser:get_field(relay_port),
+				[{mode, binary}]
+			),
+			Pid = spawn_link(fun() -> 
+					loop(
+					"", 
+					erlang:system_time(seconds),
+					Socket
+				) end
+			),
+			register(sender, Pid);
+		_ ->
+			ok
+	end,
+	whereis(sender).
 
 % Queues message for sending
-send(Message) -> sender ! Message.
+send(Message) -> 
+	sender ! Message.
 
 % Receives messages. 
 % If (now - LastTime) is 5 or greater, send queued messages
@@ -27,18 +34,21 @@ loop(Message, LastTime, Socket) ->
 	
 	% Add message to existing message
 	receive
-		M -> Message = Message ++ M
+		M -> 
+			NewMessage = Message ++ atom_to_list(M)
 	end,
 	
 	% Check time difference and dispatch if exceeded
 	I = erlang:system_time(seconds) - LastTime,
 	if
-		I >= 5 -> dispatch(Message, Socket),
-		LastTime = erlang:system_time(seconds),
-		Message = []
-	end,
-	
-	loop(Message, LastTime, Socket).
+		I >= 5 -> 
+			dispatch(Message, Socket),
+			loop("", erlang:system_time(seconds), Socket);
+		true ->
+			loop(NewMessage, LastTime, Socket)
+	end.
 
 dispatch(Message, Socket) ->
+
+	io:format("Dispatched: ~p~n", [Message]),
 	gen_tcp:send(Socket, Message).
