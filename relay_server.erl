7a92a2f1fd9868917bad20640bc0a1b808a199be
Emanuel Mellblom <emanuel.mellblom@gmail.com>
2016-11-10 20:57:47 +0100

    Add files via upload

--- relay_server.erl
+++ relay_server.erl
@@ -1,12 +1,14 @@
 -module(relay_server).
--export([start/0, start/1, process/1, connect_to_nodeServer/0]).
+-export([start/0, start/1, process/1, connect_to_nodeServer/0, connect_to_broker/0]).
 -define(defPort,1337).
 -define(pFrequency,110).
 
-%Start the relay server with the port defined above as defPort
+% Start the relay server with the port defined above as defPort, also starts the connection
+% to the Node.js server as well as the mqtt Broker.
 start() -> start(?defPort).
 start(Port) -> 
 	register(nodeprocess, connect_to_nodeServer()),
+	register(mqttprocess, connect_to_broker()), %broker
 	case gen_tcp:listen(Port,[binary,{packet,0},{active,false}]) of
 		{ok,Listensocket} -> server_loop(Listensocket);
 		{error,Reason} -> exit({Port,Reason})
@@ -29,21 +31,29 @@ process(Socket) ->
 	io:format("~p~n",[Err]),
 	gen_tcp:close(Socket).
 
-%% receive data from socket	
+%% receive incomming data from socket (Sensor packages)
 do_recv(Socket) ->
   case gen_tcp:recv(Socket, 0) of
     {ok, Bin} -> 
-    	nodeprocess!{ok, Bin};
+    	nodeprocess!{ok, Bin}, %Send to Node.js
+    	mqttprocess!{ok, Bin}; %Send to broker
     {error, closed} -> exit(closed);
     {error, econnrefused} -> exit(colsed);
     {error, Reason} -> exit(Reason)
   end.
 
-
-relay_loop(Socket) ->
+% Loop for messages to send to Node server
+nodejs_loop(Socket) ->
 	receive
-		{ok, Bin} -> gen_tcp:send(Socket, Bin), relay_loop(Socket);
-		_ -> relay_loop(Socket)
+		{ok, Bin} -> gen_tcp:send(Socket, Bin), nodejs_loop(Socket);
+		_ -> nodejs_loop(Socket)
+	end.
+
+% Loop for messages to send to Mqtt broker
+mqtt_loop(Broker) ->
+	receive
+		{ok, Bin} -> send_to_broker(Broker, Bin);
+		_ -> mqtt_loop(Broker)
 	end.
 
 % Connect to the node.js server
@@ -51,33 +61,28 @@ connect_to_nodeServer() ->
 	{ok, NodeSocket} = gen_tcp:connect({172,20,10,3}, 1337, [{mode, binary}]),
     Initmessage = "initProcess::{\"name\":\"hej\",\"type\":\"package\"}",
     gen_tcp:send(NodeSocket, Initmessage),
-    relay_loop(NodeSocket).
+    nodejs_loop(NodeSocket).
     %do_recv(Socket, NodeSocket,0).
 
-% Send data to the node.js server
-%send_to_nodeServer(NodeSocket, Data) ->
-%	gen_tcp:send(NodeSocket, Data).
-
 % Connect to the mqtt broker
-%connect_to_broker(Socket, NodeSocket) -> 
-%	{ok, Broker} = emqttc:start_link([
-%		{host, "broker.hivemq.com"},
-%		{port, 1883},
-%		{client_id, <<"testClientEmanuel">>}]),
-%		do_recv(Socket, NodeSocket, Broker).
+connect_to_broker() -> 
+	{ok, Broker} = emqttc:start_link([
+		{host, "broker.hivemq.com"},
+		{port, 1883},
+		{client_id, <<"testClientEmanuel">>}]),
+		mqtt_loop(Broker).
 
 % Send data to the Mqtt broker
-%send_to_broker(Broker, Data) -> 	
- %   	emqttc:subscribe(Broker, <<"group8">>, 0),
-  %  	emqttc:publish(Broker, <<"group8">>,Data), 
-   % 		receive
-    %    		{publish, Topic, Payload} ->
-     %      		io:format("Message Received from ~s: ~p~n", [Topic, Payload])
-   	%%		after
-      %  		5000 ->
-       %     	io:format("Error: receive timeout!~n")
-   		%	 end.
-    	%emqttc:disconnect(C).
+send_to_broker(Broker, Data) -> 	
+    	emqttc:subscribe(Broker, <<"group8">>, 0),
+    	emqttc:publish(Broker, <<"group8">>,Data), 
+    		receive
+        		{publish, Topic, Payload} ->
+           		io:format("Message Received from ~s: ~p~n", [Topic, Payload])
+   			after
+        		1000 ->
+            	io:format("Error: receive timeout!~n")
+   			 end.
 
 
 
