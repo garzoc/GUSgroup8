d130daf7ed08e86be5084953ef75ea20e50f35a7
Hawassa <destatigistu@yahoo.com>
2016-11-24 15:14:17 +0100

    Add files via upload
    send and monitor message

---
+++ package.js
@@ -0,0 +1,69 @@
+var mod=module.exports;
+var sender;
+var mqtt = require("mqtt");
+var client  = mqtt.connect('mqtt://broker.hivemq.com:1883');
+
+var packageList=new Array;
+
+mod.init=function(host){
+	 sender=host;
+	 host.api.setStaticFunction("onMessage",host);
+	 host.api.setVIP_user(host);
+	 console.log(host.isVIP);
+	 //host.api.add_custom_attribute("test",function(){return processList},host);
+	// host.interface.getClients(host);
+	 client.subscribe('group8');
+	 client.publish('group8', 'Hello mqtt');
+	 client.on('message', function (topic, message) {
+  // message is Buffer 
+  console.log(message.toString());
+  client.end();
+});
+};
+
+mod.newUser=function(user){
+	//console.log(user.isVIP);
+	//console.log(user.api.getClients(user)[0].attribute.test());
+	//console.log(user.api.getClients(user)[0].isVIP);
+}
+var counter=0;
+
+function msg(data,client){
+	var userList=global.getClients(client);
+	var msg=global.objectToString(data);
+	//console.log(msg);
+	for(var i=0;i<userList.length;i++){	
+		//console.log(msg);
+		userList[i].send('cnsl::'+msg);
+	}
+	
+}
+
+mod.onMessage=function(data,client){
+
+	//counter++;
+	//console.log(counter);
+	
+	//1478179373
+	console.log(data+"  god morgin");
+	if(data.constructor===Array && data.length>1) {
+		console.log("data is array ??????????????????????????????????????????????????????+");
+			for(var i in data) msg(i,client);
+	}else{
+		msg(data,client);
+	}
+	
+}
+
+mod.onClose=function(client){
+	if(client.isVIP){
+		console.log("host left, turning of process "+client.serverId);
+		client.publish('group8', 'Connection failed: " + message.errorMessage)');
+		var userList=global.getClients(client.verfication);
+		for(var i=0;i<userList.length;i++){	
+		
+			global.kickUser(userList[i]);
+		}
+	}
+	
+}
