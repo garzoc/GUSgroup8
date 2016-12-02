d2e815a21565c7c87590fdd2954a85ca7a5b825f
ostkaka <ost@airmail.cc>
2016-12-02 16:25:03 +0100

    fixed error

--- public/app/socket.js
+++ public/app/socket.js
@@ -2,7 +2,7 @@ var socket=new WebSocket("ws://127.0.0.1:8000/");
 
   socket.onopen=function(e){
     console.log("Establishing contact");
-    socket.send('initProcess::{"name" : "test","type":"package"}');
+    socket.send('{"use":"initProcess","context":{"name" : "boo","type":"package"}}');
 
   };
   
