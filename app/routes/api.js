202e72c4414dc06c7d0684fe81d45246bf0fba5b
Unknown <funkyfisch@gmail.com>
2016-12-05 02:02:23 +0100

    Registration routing backend
    added registration/user manipulation routing
    deleted the wildcard option since angular isnt going to use any generic url apart from the ones defined by us

--- app/routes/api.js
+++ app/routes/api.js
@@ -64,9 +64,9 @@ module.exports = function(app, express) {
 		//decode token
 		if (token) {
 			//verifies secret and checks exp
-			
+
 			jwt.verify(token, superSecret, function(err, decoded) {
-				
+
 				if(err) {
 					res.status(403).send({
 						success: false,
@@ -80,7 +80,7 @@ module.exports = function(app, express) {
 			});
 
 		}else {
-			
+
 			//if there is no token
 			//return a response of 403 (access forbidden) and an error message
 			res.status(403).send({
@@ -90,15 +90,102 @@ module.exports = function(app, express) {
 
 		}
 	});
-	//api route to get info on current user
-	apiRouter.get('/me', function(req, res) {
-		res.send(req.decoded);
-	});
 	//test route to make sure that everything is working
 	//accessed at GET http://localhost:8080/api
-	apiRouter.get('*', function(req, res) {
-		console.log("hej");
+	apiRouter.get('/', function(req, res) {
 		res.json({message: 'welcome to our api'});
+	});
+
+	// on routes that end in /users
+	// ----------------------------------------------------
+	apiRouter.route('/users')
+
+		// create a user (accessed at POST http://localhost:8080/users)
+		.post(function(req, res) {
+
+			var user = new User();		// create a new instance of the User model
+			user.name = req.body.name;  // set the users name (comes from the request)
+			user.username = req.body.username;  // set the users username (comes from the request)
+			user.password = req.body.password;  // set the users password (comes from the request)
+
+			user.save(function(err) {
+				if (err) {
+					// duplicate entry
+					if (err.code == 11000)
+						return res.json({ success: false, message: 'A user with that username already exists. '});
+					else
+						return res.send(err);
+				}
+
+				// return a message
+				res.json({ message: 'User created!' });
+			});
+
+		})
+
+		// get all the users (accessed at GET http://localhost:8080/api/users)
+		.get(function(req, res) {
+
+			User.find({}, function(err, users) {
+				if (err) res.send(err);
+
+				// return the users
+				res.json(users);
+				console.log(users);
+			});
+		});
+
+	// on routes that end in /users/:user_id
+	// ----------------------------------------------------
+	apiRouter.route('/users/:user_id')
+
+		// get the user with that id
+		.get(function(req, res) {
+			User.findById(req.params.user_id, function(err, user) {
+				if (err) res.send(err);
+
+				// return that user
+				res.json(user);
+			});
+		})
+
+		// update the user with this id
+		.put(function(req, res) {
+			console.log(req.params.user_id);
+			User.findById(req.params.user_id, function(err, user) {
+
+				if (err) res.send(err);
+
+				// set the new user information if it exists in the request
+				if (req.body.name) user.name = req.body.name;
+				if (req.body.username) user.username = req.body.username;
+				if (req.body.password) user.password = req.body.password;
+
+				// save the user
+				user.save(function(err) {
+					if (err) res.send(err);
+
+					// return a message
+					res.json({ message: 'User updated!' });
+				});
+
+			});
+		})
+
+		// delete the user with this id
+		.delete(function(req, res) {
+			User.remove({
+				_id: req.params.user_id
+			}, function(err, user) {
+				if (err) res.send(err);
+
+				res.json({ message: 'Successfully deleted' });
+			});
+		});
+
+	// api endpoint to get user information
+	apiRouter.get('/me', function(req, res) {
+		res.send(req.decoded);
 	});
 	return apiRouter;
 };
