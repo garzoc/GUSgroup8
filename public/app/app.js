f3229ef3460cad3aac13bde077cd5e24fdf43e87
ostkaka <ost@airmail.cc>
2017-01-03 18:46:42 +0100

    deleted dupes

--- public/app/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('smosApp', ['ngAnimate', 'app.routes', 'mainCtrl', 'authService']) //, 'userCtrl', 'userService'])
-
-// application configuration to integrate token into requests
-.config(function($httpProvider) {
-
-	// attach our auth interceptor to the http requests
-	$httpProvider.interceptors.push('AuthInterceptor');
-
-});
